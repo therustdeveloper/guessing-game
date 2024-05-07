842e79e95a3f02f90b059e65a4ec1a46ea907088
William Munoz <william@rustdeveloper.io>
2024-05-07 17:53:05 -0500

    Finishing the guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess:");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {guess}");
+        match io::stdin().read_line(&mut guess) {
+            Ok(_) => {
+                let guess: u32 = match guess.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => {
+                        println!("** Please enter a valid number **\n");
+                        continue
+                    }
+                };
+
+                match guess.cmp(&secret_number) {
+                    Ordering::Less => println!("Too small!\n"),
+                    Ordering::Greater => println!("Too big!\n"),
+                    Ordering::Equal => {
+                        println!("You win!");
+                        break;
+                    },
+                }
+            }
+            Err(e) => println!("error: {e}")
+        }
+    }
 }
